71b90dd151f7b26e9823c24d99f6c60eb08d6a05
AK1993 <akissi.akan@gmail.com>
2024-05-02 19:56:40 -0500

    hotfix

---
+++ hotfix.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#asking for a number
+echo "please enter a number:"
+#reading the number number
+read number
+#print number that was read
+echo $number
+#checking if the number is even or odd
+if [ $((number % 2)) -eq 0 ]
+then
+#print number
+echo "number is even"
+else
+#print number
+echo "number is odd"
+#end of if conditional
+fi
+
+
+
